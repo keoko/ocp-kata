271bbea3e34e0d92c47e0ca3ce25191e759d7756
natxo cabre <natxo.cabre@gmail.com>
2015-10-19 22:09:24 +0200

    init ocp kata

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ocp-kata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ocp_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns ocp-kata.core-test
+  (:require [clojure.test :refer :all]
+            [ocp-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
