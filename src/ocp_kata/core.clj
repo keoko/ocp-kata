d203c930d392027f6b002af2cd0dca9778c53280
natxo cabre <natxo.cabre@gmail.com>
2015-10-19 22:41:25 +0200

    second test: say 'fizz'

--- src/ocp_kata/core.clj
+++ src/ocp_kata/core.clj
@@ -1,5 +1,18 @@
 (ns ocp-kata.core)
 
+
+(defn is-a-multiple-of-3? 
+  [x]
+  (= 0 (mod x 3)))
+
+
 (defn say
   [x]
-  (str x))
+  (or (say-bizz x)
+      (str x)))
+
+(defn say-bizz [x]
+  (when (is-a-multiple-of-3? x)
+    "Fizz"))
+
+
--- test/ocp_kata/core_test.clj
+++ test/ocp_kata/core_test.clj
@@ -4,4 +4,8 @@
 
 (deftest say-the-number
   (testing "Just say the number"
-    (is (= ["1" "2" "3" "4" "5"] (map say [1 2 3 4 5])))))
+    (is (= ["1" "2" "4" "5"] (map say [1 2 4 5])))))
+
+(deftest say-fizz
+  (testing "When a number is multiple of 3, say 'Fizz'"
+    (is (every? #(= "Fizz" %) (map say [3 6 9])))))
